a93041d1e52323780cf470db464b4288b08897f9
Mikhail Pyataev <pyataevma@gmail.com>
2022-12-10 16:58:53 +0600

    assert included

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,16 @@
 import { capitalize } from "../src/capitalize.js";
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.strictEqual(capitalize('hello'),'Hello');
+
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
+
+assert.strictEqual(capitalize(''),'');
 
 console.log('Все тесты пройдены!');
